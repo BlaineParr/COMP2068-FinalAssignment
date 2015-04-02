45f2afde5f8949f33f22a588fb383e21ee8cb22f
chutch01 <chutch01@hotmail.com>
2015-04-02 11:20:34 -0400

    added constants, created pongBall class, added background image to share

--- COMP2068-FinalAssignmentTemplate1/Scripts/constants.js
+++ COMP2068-FinalAssignmentTemplate1/Scripts/constants.js
@@ -5,17 +5,22 @@ var constants;
     constants.FONT_FAMILY = "Consolas";
     constants.FONT_COLOUR = "#FFFF00";
     // Stage Constants
-    constants.SCREEN_WIDTH = 640;
-    constants.SCREEN_HEIGHT = 480;
+    constants.SCREEN_WIDTH = 800;
+    constants.SCREEN_HEIGHT = 600;
     constants.SCREEN_CENTER_WIDTH = constants.SCREEN_WIDTH * 0.5;
     constants.SCREEN_CENTER_HEIGHT = constants.SCREEN_HEIGHT * 0.5;
     constants.OCEAN_RESET_HEIGHT = 960;
     // Game Constants
     constants.PLAYER_LIVES = 5;
     constants.CLOUD_NUM = 3;
+
     // States Constants
     constants.MENU_STATE = 0;
     constants.PLAY_STATE = 1;
+    constants.LEVEL_1_1 = 2;
+    constants.LEVEL_1_2 = 3;
+    constants.LEVEL_1_3 = 4;
+    constants.LEVEL_1_4 = 5;
     constants.GAME_OVER_STATE = 2;
 })(constants || (constants = {}));
 //# sourceMappingURL=constants.js.map
---
+++ COMP2068-FinalAssignmentTemplate1/Scripts/objects/pongBall.ts
@@ -0,0 +1,39 @@
+﻿/*
+* this is the laser that the player fires from the character it will be removed if it collides with something
+*/
+module objects {
+    export class PongBall extends objects.GameObject {
+
+      
+
+        //constructor ++++++++++++++++++++++++++++
+        constructor(x: number, y: number) {
+            super("pongBall");
+            this.x = x;
+            this.y = y;
+  
+
+
+            //this.soundString = "laser_sound";
+
+
+
+        }
+
+        //public methods+++++++++++++++++++++++++++
+        public update() {
+
+            this.x += 5;
+
+            if (this.x > 800) {
+
+            }
+
+        }
+        public hit() {
+            
+
+        }
+
+    }
+}  

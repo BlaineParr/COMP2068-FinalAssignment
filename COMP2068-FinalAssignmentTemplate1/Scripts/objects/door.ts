571ec40c7be350e11fb4ebcac3d010d278f36528
chutch01 <chutch01@hotmail.com>
2015-04-02 11:56:34 -0400

    created door object

---
+++ COMP2068-FinalAssignmentTemplate1/Scripts/objects/door.ts
@@ -0,0 +1,33 @@
+﻿module objects {
+
+    export class Door extends objects.GameObject {
+
+          public lockedUnlocked: boolean;
+
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++
+        constructor() {
+            super("door");
+            this.name = "door";
+            //this.soundString = "thunder";
+          
+        }
+
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++
+      
+
+        
+
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+
+        public hit() {
+            
+
+
+      
+        }
+
+
+    }
+
+}    
